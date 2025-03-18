067793d4d450bf2bf91ac0646cdf1b61526be080
cy <jcy1236@naver.com>
2025-03-18 17:00:22 +0900

    1st commit

---
+++ PrismApp/PrismApp.Core/RegionNames.cs
@@ -0,0 +1,7 @@
+﻿namespace PrismApp.Core
+{
+    public static class RegionNames
+    {
+        public const string ContentRegion = "ContentRegion";
+    }
+}
---
+++ PrismApp/PrismApp/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using PrismApp.Modules.ModuleName;
+using PrismApp.Services;
+using PrismApp.Services.Interfaces;
+using PrismApp.Views;
+using System.Windows;
+
+namespace PrismApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterSingleton<IMessageService, MessageService>();
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            moduleCatalog.AddModule<ModuleNameModule>();
+        }
+    }
+}
